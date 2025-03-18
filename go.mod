3a811a56e8c6060be1a34a19cfbc3592e01cf5b9
canas <chakir.anas@gmail.com>
2025-03-18 11:46:23 +0100

    kk

--- go.mod
+++ go.mod
@@ -1,5 +1,9 @@
 module forum
 
-go 1.22.2
+go 1.23.0
+
+toolchain go1.24.1
 
 require github.com/mattn/go-sqlite3 v1.14.24
+
+require golang.org/x/crypto v0.36.0 // indirect
--- main.go
+++ main.go
@@ -19,4 +19,39 @@ func main() {
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/logout", logout)
 	http.HandleFunc("/protected", protected)
-	
+	http.ListenAndServe(":8080", nil)
+}
+
+func register(w http.ResponseWriter, r *http.Request) {}
+	if r.Method != http.MethodPost {
+		er := http.StatusMethodNotAllowed
+		http.error(w, "Invalid method", er)
+		return
+	}
+
+	username := rFormValue("username")
+	password := rFormValue("password")
+	if len(username)<8 || len(password)<8 {
+		er := http.StatusNotAcceptable
+		http.error(w, "Invalid username or password", er)
+		return
+	}
+
+	if _, ok := users[username]; ok {
+		er := http.StatusConflict
+		http.error(w, "Username already exists", er)
+		return
+	}
+
+	hashPassword, _ := hashPassword(password)
+	users[username] = login{
+		hashedPassword: hashPassword,
+	}
+
+	fmt.Fprintf(w, "User registered successfully ! ")
+
+func login(w http.ResponseWriter, r *http.Request) {}
+
+func logout(w http.ResponseWriter, r *http.Request) {}
+
+func protected(w http.ResponseWriter, r *http.Request) {}
