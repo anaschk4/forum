ec22591b23d954e19810866dd3afc1afb9da2e6e
canas <chakir.anas@gmail.com>
2025-03-18 11:11:28 +0100

    kk

---
+++ database.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3" 
+)
+
+var DB *sql.DB
+
+func ConnectDB() {
+	var err error
+	DB, err = sql.Open("sqlite3", "forum.db")
+	if err != nil {
+		log.Fatal("Erreur lors de l'ouverture de la base de données :", err)
+	}
+
+	
+	err = DB.Ping()
+	if err != nil {
+		log.Fatal("Impossible de se connecter à la base de données :", err)
+	}
+
+	fmt.Println("✅ Connexion à la base de données réussie !")
+
+	
+	createTable := `CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		email TEXT NOT NULL UNIQUE,
+		password TEXT NOT NULL
+	);`
+	_, err = DB.Exec(createTable)
+	if err != nil {
+		log.Fatal("Erreur lors de la création de la table :", err)
+	}
+}
